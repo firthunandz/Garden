7fc2caa2b36f0cfd5040e066d07fc2edb1775c7b
firthunandz <fernandonchiesa@gmail.com>
2026-06-19 21:46:21 -0300

    astro.config base fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://firthunandz.github.io',
-  base: '/garden',
+  base: '/Garden',
   output: 'static',
 });
